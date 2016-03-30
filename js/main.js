b1a6e9e188a4ea8961cbcbe86ebd8a2376ed5984
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-03-29 20:17:39 -0400

    add class

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var $svg = $('svg');
+var $lines = $('.lines');
+var $btn = $('.btn');
+
+$btn.on('click', function () {
+  $lines.addClass('draw');
+  $lines.removeClass('lines');
+});
+
+$btn.on('click', function () {
+  if ($svg.hasClass('is-engaged')) {
+    $svg.addClass('is-disengaged');
+    $svg.removeClass('is-engaged');
+  } else {
+    $svg.addClass('is-engaged');
+    $svg.removeClass('is-disengaged');
+  }
+});
